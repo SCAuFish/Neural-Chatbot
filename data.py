b4e787f01b6fb78b475f93a52e610c3a4ecc38f9
SCAuFish <shenchg126@gmail.com>
2019-05-21 01:37:59 -0700

    reading functunctions in progress

---
+++ data.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+# This file contains method to read in lines from Cornell movie lines dataset
+# Cheng Shen May 9th 2019
+
+import re
+
+class TextReader():
+	def __init__(self):
+		# Check read_line_dict for the lines intance variable
+		self.lines     = dict()
+		self.dialogues = []
+
+	def index_to_num(self, index):
+		# transform the line index into an int index
+		# line index is in the format "L###"
+		return int(index[1:])
+
+	def read_line_dict(self, input_file_name="./Data/cornell_data/movie_lines.txt"):
+		# Read in line-dict from "movie_lines.txt"
+		input_file = open(input_file_name, "rb")
+
+		for line in input_file:
+			parts    = line.strip().split(b" +++$+++ ")  # separator in cornell dataset
+			index    = self.index_to_num(parts[0])   # original format: "LXXX"
+			sentence = parts[-1]
+
+			self.lines[index] = sentence
+
+	def read_dialogues(self, input_file_name="./Data/cornell_data/movie_conversations.txt"):
+		# Read in all dialogues stored in 
+		input_file = open(input_file_name, "rb")
+
+		for line in input_file:
+			parts    = line.strip().split(b" +++$+++ ")
+			dialogue = parts[-1]
+
+			self.dialogues = [[]] + self.dialogues
+
+			for s in dialogues:
+				s = re.sub(r'[^\w\s]','',s).strip()
+				self.dialogues[0].append(s)
---
+++ log.md
@@ -0,0 +1,15 @@
+# Log for the Project
+In order to manage this project and record possible ideas, I create this
+document to better organize (hopefully).
+
+## May. 7th 2019
+Decided to work on the neural chatbot frame work proposed by Jiwei Li et al in Deep Reinforcement Learning for Dialogue Generation. Other interesting topics involve target-based conversation agent, like the one based on POMDP proposed by Jason Williams et al in Partially Observable Markov Decision Processes for Spoken Dialog Systems.
+Today try to collect data used in the paper--OpenSubtitles dataset. And it's crazy because it's large and not clean.
+Therefore I am using the Cornell dataset given here: http://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html to try training a seq2seq model first.
+It's much smaller. Let's write a simple reader for it.
+
+## May. 20th 2019
+It's been 13 days since last time I did this!
+It's too late tonight and I finished the reader which can read line
+by line.
+read_dialogues not tested yet
